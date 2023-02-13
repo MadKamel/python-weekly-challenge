9d3f74e4fcc699043c1e072a9c7f4d5a30ecfb02
Eric Rustrum <rustrumek@gmail.com>
2023-02-13 08:22:42 -0800

    Update main.py

--- week-1/main.py
+++ week-1/main.py
@@ -1,1 +1,20 @@
 # Command-line calculator app.
+#   Written by MadKamel
+
+def is_int(string):
+    try:
+        return type(int(string)) == int
+    except:
+        return False
+
+
+
+if __name__ == '__main__':
+
+    while True:
+        # get input, split by spaces.
+        uIn = input(':> ').split(' ')
+
+        for i in range(len(uIn)):
+            if is_int(uIn[i]):
+                
